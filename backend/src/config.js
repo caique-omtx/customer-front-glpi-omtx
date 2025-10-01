b75991d9dd78773e7ba70c2803dfff1d5bdd7041
caique-omtx <caique@omtx.com.br>
2025-10-01 17:09:08 -0300

    Correção do request

--- backend/src/config.js
+++ backend/src/config.js
@@ -4,5 +4,5 @@ dotenv.config();
 // Exporta configurações do backend
 export const PORT = process.env.PORT || 5000; // Porta do servidor Express
 export const CORS_ORIGIN = process.env.CORS_ORIGIN || '*'; // Origem permitida para CORS
-export const GLPI_API_URL = process.env.GLPI_API_URL || 'http://omtx.dyndns.org:8257/glpi/apirest.php'; // URL da API GLPI
-export const GLPI_APP_TOKEN = process.env.GLPI_APP_TOKEN || 'jmJ5LHOhPwV9wOhRKfR0tphAPpFYpyoLgmyq0dS7'; // Token do aplicativo GLPI
+export const GLPI_API_URL = process.env.GLPI_API_URL || 'XXXXXXXXXXXXXXX'; // URL da API GLPI
+export const GLPI_APP_TOKEN = process.env.GLPI_APP_TOKEN || 'XXXXXXXXXXXXXX'; // Token do aplicativo GLPI
